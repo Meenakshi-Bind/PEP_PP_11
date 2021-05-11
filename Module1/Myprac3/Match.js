2b17261f24abd3d4db13d67645f5c2f29c97a2b6
Meenakshi Bind <Meenakshibind@gmail.com>
2021-05-11 21:23:02 +0530

    Leaderboard Mprac

--- Module1/Myprac3/Match.js
+++ Module1/Myprac3/Match.js
@@ -2,11 +2,16 @@ const request=require("request");
 const cheerio=require("cheerio");
 const fs=require("fs");
 let matchlink="https://www.espncricinfo.com/series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
+let leaderboard = [];
+let countOfRequestSent=0;
 
 request(matchlink,function(error,reponse,data)
 {
     processData(data);
     // console.log(data);
+    if(countOfRequestSent == 0){
+        console.table(leaderboard);
+    }
 })
 
 function processData(html)
@@ -32,12 +37,46 @@ for(let i=0;i<B_Innings.length;i++)
            let sixes=myDocument(Alltds[6]).text().trim();
            let strike_rate=myDocument(Alltds[7]).text().trim();
            console.log(`Batsman=${Batsman_Name} Runs=${runs} Balls=${balls} Fours=${fours} Sixes=${sixes} Strike_rate=${strike_rate}`);
+           processLeaderBoard(teamName , Batsman_Name, runs , balls , fours , sixes);
         }
        
     }
+   
     
-    console.log("########################################################################");
+    //console.log("########################################################################");
 }
+function processLeaderBoard(teamName , Batsman_Name , runs , balls , fours , sixes ){
+    runs = Number(runs);
+    balls = Number(balls);
+    fours = Number(fours);
+    sixes = Number(sixes);
+
+    for(let i=0;i<leaderboard.length ; i++){
+        let batsmanObject = leaderboard[i];
+        
+        if(batsmanObject.Team == teamName && batsmanObject.Batsman == Batsman_Name){
+            batsmanObject.Runs = batsmanObject.Runs + runs;
+            batsmanObject.Balls += balls;
+            batsmanObject.Fours += fours;
+            batsmanObject.Sixes += sixes;
+            return;
+        }
+    }
+
+    let batsmanObject = {
+        Team : teamName ,
+        Batsman : Batsman_Name ,
+        Runs : runs ,
+        Balls : balls , 
+        Fours : fours , 
+        Sixes : sixes
+    }
+    leaderboard.push(batsmanObject);
+}
+
+
+
+
 
 }
 
