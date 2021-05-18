af2d240339684d42318ad6fa2dacfdc2d07f26df
Meenakshi Bind <Meenakshibind@gmail.com>
2021-05-18 11:38:21 +0530

    concepts hoisting closure etc

---
+++ Module1/Lec7/hoisting.js
@@ -0,0 +1,12 @@
+// undefined
+console.log(a);
+var a = "Steve";
+console.log(a);
+
+// not defined
+console.log(c);
+
+// cannot access a before initialization
+console.log(b);
+let b = "Steve";
+console.log(b);
