859ab2510ee2d5f5bb1a9186795786b76e0252f7
Meenakshi Bind <Meenakshibind@gmail.com>
2021-10-11 15:00:48 +0530

    Car Parking App

---
+++ EPIK/script.js
@@ -0,0 +1,57 @@
+ 
+ setTimeout(function() {
+    let OUTcheck=document.querySelectorAll("button.buttonOut");
+    console.log(OUTcheck.length+"dff")
+    for(let i=0;i<OUTcheck.length;i++)
+    {
+        OUTcheck[i].addEventListener("click",function(e){
+            
+            aftercheckout.style.removeProperty("display");
+           
+            
+                block.style.display = "none";
+                let table2=document.querySelector(".popCheckOut");
+                let telement2=document.createElement("tr");
+                telement2.innerHTML=`<tr>
+                <td>${List[i].NameCus}</td>
+                <td>${List[i].Contact}</td>
+                <td>${List[i].type}</td>
+                <td>${List[i].VehicleMODEL}</td>
+               
+            </tr> ` 
+            let table3=document.querySelector(".popCheckOut2");
+            let telement3=document.createElement("tr");
+            telement3.innerHTML=`<tr> 
+            <td>${List[i].VehicleTYPE}</td>
+            <td>${List[i].CheckINDate}</td>
+            <td>${new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })}</td>
+            <td>${(((new Date().getTime()-List[i].Duration)/1000)/60)/60}hrs</td>
+            </tr>`
+            table2.append(telement2);
+            table3.append(telement3);
+       
+            
+        })
+    }
+}, 1000);
+ 
+ 
+ let buttonPro=document.querySelector("button.Pro");
+
+ buttonPro.addEventListener("click",function(){
+    aftercheckout.style.display = "none";
+    block.style.display = "";
+    pay.style.display = "";
+ })
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+       
---
+++ EPIK/scriptdb.js
@@ -0,0 +1,106 @@
+let pay=document.querySelector(".PaymentDone")
+// window.location.reload();
+let btn=document.querySelector("button.addContent");
+let block=document.querySelector(".block");
+let submit=document.querySelector(".submit");
+let aftercheckout=document.querySelector(".afterCheckOut");
+let popup=document.querySelector(".popCheckIn");
+
+btn.addEventListener("click",function(){
+    block.style.display = "none";
+    popup.style.removeProperty("display");
+    
+    
+    
+    
+    
+})
+
+let List=[];
+
+submit.addEventListener("click",function(){
+    let VehicleType=document.querySelector("select#cars");
+    let VehicleNo=document.querySelector("input#VehicleNo");
+    let phone=document.querySelector("input#Phoneno");
+    let VehicleModel=document.querySelector("input#VehicleModel");
+    let customer=document.querySelector("input#CustomerName");
+    addMedia(VehicleType.value,VehicleNo.value,customer.value,phone.value,VehicleModel.value);
+    window.location.reload();
+})
+
+
+let db;
+
+let dbOpenRequest = indexedDB.open("Car", 2);
+dbOpenRequest.onupgradeneeded = function (e) {
+  db = e.target.result;
+  db.createObjectStore("CAR_NAME", { keyPath: "mid" }); // table will only be create when db is create first time
+};
+dbOpenRequest.onsuccess = function (e) {
+  db = e.target.result;
+  fetchMedia();
+};
+dbOpenRequest.onerror = function (e) {
+  alert("Inside on error !!");
+};
+
+
+function addMedia(VehicleType,VehicleNo,customer,phone,VehicleModel) {
+    //   db me media add hojaega
+    let txnObject = db.transaction("CAR_NAME", "readwrite"); // start transaction on mediaTable
+    let NameTable = txnObject.objectStore("CAR_NAME"); // this will get access to mediaTable
+  
+    NameTable.add({ mid: Date.now(), type: VehicleNo,VehicleMODEL:VehicleModel,VehicleTYPE:VehicleType,Duration:new Date().getTime(), Contact : phone,NameCus:customer,CheckINDate:new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })}); // it will add this object in mediaTable or mediaStore
+  
+    txnObject.onerror = function (e) {
+      console.log("txn failed");
+      console.log(e);
+      window.location.reload();
+    };
+  }
+  function fetchMedia() {
+    let txnObject = db.transaction("CAR_NAME", "readonly");
+    let NameTable = txnObject.objectStore("CAR_NAME");
+    let cursorObject = NameTable.openCursor(); // to iterate on all the rows / tuples
+    cursorObject.onsuccess = function (e) {
+      let cursor = cursorObject.result;
+      if (cursor) {
+        let NameObj = cursor.value;
+        
+        
+        
+        cursor.continue();
+        
+        append(NameObj,List);
+      }
+    };
+  }
+  function append(NameObj,List)
+        {
+       List.push(NameObj);
+      console.log(List)
+        let table=document.querySelector(".table") 
+        
+        let telement=document.createElement("tr");
+        telement.innerHTML=`<tr >
+        <th>${NameObj.type}</th>
+        <th>${NameObj.VehicleMODEL}</th>
+        <th>${NameObj.VehicleTYPE}</th>
+        <th>${NameObj.Contact}</th>
+        <th>${NameObj.CheckINDate}</th>
+        <button class="buttonOut"><i class="fas fa-minus"></i>Check-Out</button>
+       </tr>`
+        table.append(telement);
+        
+       
+        
+        
+        
+        }
+        
+        
+        let close=document.querySelector(".closeButton");
+        close.addEventListener("click",function(){
+         
+          pay.style.display = "none";
+        })
